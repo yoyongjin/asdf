7b29fc1a93cc7db51a40ff9f98d70218233f65ed
gingaminga <gingahae@gmail.com>
2021-09-10 16:02:45 +0900

    env 타입 지정

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_API_SERVER: string;
+    REACT_APP_SOCKET_SERVER: string;
+    REACT_APP_COMPANY: string;
+    REACT_APP_MODE: string;
+  }
+}
