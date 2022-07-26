46466cd5445fb558c13bff3ff1c97a05a72ded64
gingaminga <gingahae@gmail.com>
2022-07-26 16:46:18 +0900

    Refactor: 버튼 순서 변경

--- src/components/molecules/BatchItem.tsx
+++ src/components/molecules/BatchItem.tsx
@@ -17,7 +17,7 @@ const buttonData = [
     fontWeight: 800,
     id: 1,
     height: 5,
-    value: '조직 인사 수동 배치',
+    value: '법인폰 일광 등록 배치',
     width: 20,
   },
   {
@@ -29,7 +29,7 @@ const buttonData = [
     fontWeight: 800,
     id: 2,
     height: 5,
-    value: 'KSVC 수동 배치',
+    value: '조직 인사 수동 배치',
     width: 20,
   },
   {
@@ -53,7 +53,7 @@ const buttonData = [
     fontWeight: 800,
     id: 4,
     height: 5,
-    value: '법인폰 수동 배치',
+    value: 'KSVC 수동 배치',
     width: 20,
   },
 ];
@@ -84,17 +84,17 @@ function BatchItem() {
         let text = '';
 
         if (values.id === 1) {
+          // 법인폰 일괄 등록 배치
+          onClick = syncPhoneInfoStatus
+            ? () => null
+            : _.debounce(() => handleSyncPhoneInfo(), 1000);
+          text = syncPhoneInfoStatus ? '처리중...' : values.value;
+        } else if (values.id === 2) {
           // 조직, 인사 수동 배치
           onClick = syncBatchUserStatus
             ? () => null
             : _.debounce(() => handleSyncBranchAndUser(), 1000);
           text = syncBatchUserStatus ? '처리중...' : values.value;
-        } else if (values.id === 2) {
-          // KSVC 수동 배치
-          onClick = syncKSVCStatus
-            ? () => null
-            : _.debounce(() => handleSyncKSVC(), 1000);
-          text = syncKSVCStatus ? '처리중...' : values.value;
         } else if (values.id === 3) {
           // IP 수동 배치
           onClick = syncIPStatus
@@ -102,11 +102,11 @@ function BatchItem() {
             : _.debounce(() => handleSyncIP(), 1000);
           text = syncIPStatus ? '처리중...' : values.value;
         } else if (values.id === 4) {
-          // phone info 수동 배치
-          onClick = syncPhoneInfoStatus
+          // KSVC 수동 배치
+          onClick = syncKSVCStatus
             ? () => null
-            : _.debounce(() => handleSyncPhoneInfo(), 1000);
-          text = syncPhoneInfoStatus ? '처리중...' : values.value;
+            : _.debounce(() => handleSyncKSVC(), 1000);
+          text = syncKSVCStatus ? '처리중...' : values.value;
         }
 
         return (
