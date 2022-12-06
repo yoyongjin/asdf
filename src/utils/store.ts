8d875d5c4e254cf47069b29bb89b23f4c06eae6b
yoyongjin <yongjinalize@gmail.com>
2022-12-06 16:48:16 +0900

    refactor: migrate createStore to configureStore

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -2,6 +2,7 @@ import { createBrowserHistory } from 'history';
 import { applyMiddleware, createStore, Store, StoreEnhancer } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from 'modules/reducers';
 import rootSaga from 'modules/sagas';
@@ -17,21 +18,31 @@ const rootStore = () => {
     },
   });
 
-  let middleware: StoreEnhancer | undefined;
+  // redux 레거시(createStore)
+  // let middleware: StoreEnhancer | undefined;
 
-  if (constants.NODE_ENV === 'development') {
-    // 개발툴 적용
-    middleware = composeWithDevTools(applyMiddleware(sagaMiddleware));
-  } else if (constants.NODE_ENV === 'production') {
-    middleware = applyMiddleware(sagaMiddleware);
-  }
+  // if (constants.NODE_ENV === 'development') {
+  //   // 개발툴 적용
+  //   middleware = composeWithDevTools(applyMiddleware(sagaMiddleware));
+  // } else if (constants.NODE_ENV === 'production') {
+  //   middleware = applyMiddleware(sagaMiddleware);
+  // }
 
-  if (!middleware) {
-    return;
-  }
+  // if (!middleware) {
+  //   return;
+  // }
 
-  const store = createStore(rootReducer, middleware) as Store;
-  sagaMiddleware.run(rootSaga);
+  // const store = createStore(rootReducer, middleware) as Store;
+  // sagaMiddleware.run(rootSaga);
+
+  // redux-toolkit(configureStore)
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: [sagaMiddleware],
+    // devTools 옵션: devlopment|productioni|test
+    devTools: process.env.NODE_ENV !== 'development',
+  });
+  sagaMiddleware?.run(rootSaga);
 
   return store;
 };
